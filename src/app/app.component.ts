1b0618c56cdc4a4f14ba1a6dc87e648bb174cace
Prateek Mehta <pattmehta.official@gmail.com>
2024-11-07 19:17:02 -0800

    adds two way binding files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 
 import { InnerTextDemoComponent } from './inner-text-demo/inner-text-demo.component';
 import { StructuralDirectivesComponentComponent } from './structural-directives-component/structural-directives-component.component';
+import { TwoWayBindingExampleComponent } from './two-way-binding-example/two-way-binding-example.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     InnerTextDemoComponent,
-    StructuralDirectivesComponentComponent
+    StructuralDirectivesComponentComponent,
+    TwoWayBindingExampleComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
